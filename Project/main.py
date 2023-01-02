a3126d45978e8f245f387ee6dd4ff619068a7ba1
Andrei-Călin Ciută <andreiciuta.ac@gmail.com>
2023-01-02 22:53:50 +0200

    Greedy

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 # FII - AI Project
 # Ciuta Andrei Calin
@@ -6,66 +7,79 @@ from tkinter import *
 # Tablan Andrei Razvan
 # Volentir Alexandra
 
-root = Tk()
-myLabel = Label(root, text="Welcome!")
-myLabel.pack()
-root.mainloop()
+# root = Tk()
+# myLabel = Label(root, text="Welcome!")
+# myLabel.pack()
+# root.mainloop()
 
-'''
-class Person:
-    def __init__(self, id, preferences, count):
-        self.id = id
-        self.married = False
-        self.preferences = preferences
-        self.state = [0 for x in range(0, count)]
-
-    def set_married(self):
-        self.married = True
-'''
+def get_rank(list_of_preferences, person):
+    return list_of_preferences.index(person)
 
 
-def is_stable(states, preferences_men, preferences_women, men, women):
-    wives = []
-    for man1 in preferences_men.keys():
-        for man2 in preferences_men.keys():
-            for index in range(0, len(states[man1])):
-                if states[man1][index] == 1:
-                    woman = women[index]
-                    print(man1, ' ', woman)
+
+def greedy_approach(men, women, preferences_men, preferences_women):
+    # Storing the number of men and women
+    number_of_men = len(men)
+    number_of_women = len(women)
+
+    # Keeping a list of unmarried men
+    list_of_unmarried_men = men
+
+    # As default, each person is single
+    partner_man = {}
+    for man in men:
+        partner_man[man] = None
+    partner_woman = {}
+    for woman in women:
+        partner_woman[woman] = None
+    
+    # Keep the next step of each men
+    next_man_choice = {}
+    for man in men:
+        next_man_choice[man] = 0
+    
+    # State is the number of couples, when we have n couples, a list will be returned
+    state = 0
+
+    while state < number_of_men:
+        # Choose a random man
+        random_man = random.choice(list_of_unmarried_men)
+        # Take his actual preference
+        preference_man = preferences_men[random_man][next_man_choice[random_man]]
+        # Check actual woman partner
+        actual_partner_woman = partner_woman[preference_man]
+
+        if actual_partner_woman is None:
+            # If she is single, we can create a couple
+            list_of_unmarried_men.remove(random_man)
+            partner_woman[preference_man] = random_man
+            partner_man[random_man] = preference_man
+            next_man_choice[random_man] += 1
+
+            # Now we have one more couple
+            state += 1
+        else:
+            # She already has a partner
+            # Check the rank of his actual partner
+
+            partner_rank = get_rank(preferences_women[preference_man], actual_partner_woman)
+            random_man_rank = get_rank(preferences_women[preference_man], random_man)
+
+            if random_man_rank > partner_rank:
+                # She gets a new partner
+                list_of_unmarried_men.remove(random_man)
+                list_of_unmarried_men.append(actual_partner_woman)
+                partner_woman[preference_man] = random_man
+                partner_man[random_man] = preference_man
+                next_man_choice[random_man] += 1
+            else:
+                next_man_choice[random_man] += 1
+    return partner_man, partner_woman
 
 
-def update_states(states, preferences_men, preferences_women, man, woman, index_woman, index_man):
-    states[man][index_woman] = 1
-    states[woman][index_man] = 1
-
-    for man_key in preferences_men.keys():
-        if man_key != man:
-            states[man_key][index_woman] = -1
-    for woman_key in preferences_women.keys():
-        if woman_key != woman:
-            states[woman_key][index_man] = -1
-    return states
 
 
-def greedy_by_men(states, preferences_men, preferences_women):
-    for key, value in preferences_men.items():
-        for preference in value:
-            index_woman = list(preferences_women).index(preference)
-            index_man = list(preferences_men).index(key)
-            if states[key][index_woman] == 0:
-                states = update_states(states, preferences_men, preferences_women, key, preference, index_woman,
-                                       index_man)
-                break
-    return states
 
-
-def create_states(preferences_men, preferences_women):
-    states = {}
-    for key in preferences_men.keys():
-        states[key] = [0 for x in range(0, len(preferences_women.keys()))]
-    for key in preferences_women.keys():
-        states[key] = [0 for x in range(0, len(preferences_men.keys()))]
-    return states
 
 
 def create_person_list(dictionary):
@@ -87,15 +101,16 @@ def read_input(path):
     return preferences
 
 
-def solve_problem():
+def solve_problem(preferences_men, preferences_women, solving_metho):
     preferences_men = read_input("input_men.txt")
     preferences_women = read_input("input_women.txt")
     men = create_person_list(preferences_men)
     women = create_person_list(preferences_women)
-    states = create_states(preferences_men, preferences_women)
-    states = greedy_by_men(states, preferences_men, preferences_women)
-    print(states)
-    is_stable(states, preferences_men, preferences_women, men, women)
+
+    if solving_metho == 'Greedy':
+        print(greedy_approach(men, women, preferences_men, preferences_women))
+    else:
+        pass
 
 
 if __name__ == '__main__':
