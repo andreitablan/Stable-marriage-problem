275b62a29ce3cb59afd5d26bc210cfe74d6b427f
Andrie Tablan <andreitablan01@gmail.com>
2023-01-11 17:30:55 +0200

    complete code

--- Project/main.py
+++ Project/main.py
@@ -136,7 +136,7 @@ def choose_preferences(number_of_couples, solving_method):
             index += 1
 
         if input_from_user_is_correct(preferences_men, preferences_women, number_of_couples) is True:
-            men_list, women_list = solve_problem(preferences_men, preferences_women, solving_method)
+            men_list, women_list = solve_problem(number_of_couples,preferences_men, preferences_women, solving_method)
             show_solution(men_list, number_of_couples, solving_method)
         else:
             Label(f5, text="The input is incorrect!", borderwidth=0, bg="#FFBBBC", fg='#F04755',
@@ -187,7 +187,7 @@ def random_preferences(number_of_couples, solving_method):
     for woman in list_of_women:
         random.shuffle(list_of_men)
         preferences_women[woman] = list(list_of_men)
-    men_list, women_list = solve_problem(preferences_men, preferences_women, solving_method)
+    men_list, women_list = solve_problem(number_of_couples,preferences_men, preferences_women, solving_method)
     show_solution(men_list, number_of_couples, solving_method)
 
 
@@ -354,8 +354,7 @@ def description_page():
                "preferences and which algorithm our app should use\n(Greedy or Backtracking).\n\n➋ Second, the program " +
                "runs the algorithm for the given instance and finds a \nresult, considering every person's preferences." +
                "\n\n➌ Finally, the result is displayed on the app's screen.",
-          borderwidth=0, bg="#FFBBBC", fg='#F04755', font=('Montserrat 20'), justify=LEFT).pack(side=LEFT, anchor=N,
-                                                                                                padx=50, pady=0)
+          borderwidth=0, bg="#FFBBBC", fg='#F04755', font=('Montserrat 20'), justify=LEFT).place(x=40,y=300)
     back_arrow = PhotoImage(file="back_arrow.png")
 
     back_btn = Button(f2, text="Back", borderwidth=0, bg="#FFBBBC", fg='#000000', font='Montserrat 14 bold',
@@ -365,16 +364,244 @@ def description_page():
     root.mainloop()
 
 
-def solve_problem(preferences_men, preferences_women, solving_method):
-    pass
-
-
 def start_app():
     for frame in (f1, f2, f3, f4, f5, f6):
         frame.grid(row=0, column=0, sticky='news')
     home()
 
 
+'''----------GREEDY----------'''
+
+
+def get_rank(list_of_preferences, person):
+    return list_of_preferences.index(person)
+
+
+def greedy_approach(men, women, preferences_men, preferences_women):
+    print("GREEDY")
+    print(preferences_men)
+    print(preferences_women)
+    print(men)
+    print(women)
+    # Storing the number of men and women
+    number_of_men = len(men)
+    number_of_women = len(women)
+
+    # Keeping a list of unmarried men
+    list_of_unmarried_men = men
+
+    # As default, each person is single
+    partner_man = {}
+    for man in men:
+        partner_man[man] = None
+    partner_woman = {}
+    for woman in women:
+        partner_woman[woman] = None
+
+    # Keep the next step of each men
+    next_man_choice = {}
+    for man in men:
+        next_man_choice[man] = 0
+
+    # State is the number of couples, when we have n couples, a list will be returned
+    state = 0
+
+    while state < number_of_men:
+        # Choose a random man
+        random_man = random.choice(list_of_unmarried_men)
+        # Take his actual preference
+        preference_man = preferences_men[random_man][next_man_choice[random_man]]
+        # Check actual woman partner
+        actual_partner_woman = partner_woman[preference_man]
+
+        if actual_partner_woman is None:
+            # If she is single, we can create a couple
+            list_of_unmarried_men.remove(random_man)
+            partner_woman[preference_man] = random_man
+            partner_man[random_man] = preference_man
+            next_man_choice[random_man] += 1
+
+            # Now we have one more couple
+            state += 1
+        else:
+            # She already has a partner
+            # Check the rank of his actual partner
+
+            partner_rank = get_rank(preferences_women[preference_man], actual_partner_woman)
+            random_man_rank = get_rank(preferences_women[preference_man], random_man)
+
+            if random_man_rank > partner_rank:
+                # She gets a new partner
+                list_of_unmarried_men.remove(random_man)
+                list_of_unmarried_men.append(actual_partner_woman)
+                partner_woman[preference_man] = random_man
+                partner_man[random_man] = preference_man
+                next_man_choice[random_man] += 1
+            else:
+                next_man_choice[random_man] += 1
+    return partner_man, partner_woman
+
+
+def create_person_list(dictionary):
+    list_of_people = []
+    for key in dictionary.keys():
+        list_of_people.append(key)
+    return list_of_people
+
+
+def read_input(path):
+    preferences = {}
+    with open(path) as file:
+        lines = [line.rstrip() for line in file]
+    for line in lines:
+        person = line.split(":")
+        preference_by_person = person[1].split(",")
+        preferences[person[0]] = preference_by_person
+    file.close()
+    return preferences
+
+'''
+-------------------BKT-----------------------
+'''
+
+N = 0
+states = list()
+solutions_bkt = list()
+output_solutions = list()
+count = 0
+counter = 0
+
+
+def ok(q_dict: list, col: int, mp, wp):
+    for i in range(0, col):
+        if q_dict[col] == q_dict[i]:
+            return False
+
+    for i in range(0, col):
+        if q_dict[col] == q_dict[i]:
+            return False
+
+    for i in range(0, col):
+        if (mp[i][q_dict[col]] < mp[i][q_dict[i]]) and (wp[q_dict[col]][i] < wp[q_dict[col]][col]):
+            return False
+        if (mp[col][q_dict[i]] < mp[col][q_dict[col]]) and (wp[q_dict[i]][col] < wp[q_dict[i]][i]):
+            return False
+    return True
+
+
+def show(q_dict):
+    global count
+    count += 1
+    for i in range(0, N):
+        for j in range(0, N):
+            if q_dict[i] == j:
+                print("Man ", i, " is matched with woman ", j)
+
+
+def get_state(q_dict, sol):
+    global count
+    state = dict()
+    count += 1
+    for i in range(0, N):
+        for j in range(0, N):
+            if q_dict[i] == j:
+                state[i] = j
+    return state, sol
+
+
+def append_state(q_dict, sol):
+    global states
+    states.append((get_state(q_dict, sol)))
+
+
+def append_to_output_possible_solutions(q_dict):
+    global output_solutions
+    global counter
+    state = dict()
+    counter += 1
+    for i in range(0, N):
+        for j in range(0, N):
+            if q_dict[i] == j:
+                state[str(i + 1)] = chr(ord('@') + j + 1)
+                # print(state)
+    output_solutions.append(state)
+
+
+def move(q_dict, i, mp, wp):
+    # create state
+    if i == N:
+        append_state(q_dict, 1)
+        # print("q", q_dict)
+        solutions_bkt.append(get_state(q_dict, 1))
+        append_to_output_possible_solutions(q_dict)
+        # show(q_dict)
+        return
+    append_state(q_dict, 0)
+    for j in range(0, N):
+        q_dict[i] = j
+        if ok(q_dict, i, mp, wp):
+            move(q_dict, i + 1, mp, wp)
+
+
+def bkt_approach(couples, mp, wp):
+    global N
+    global states
+    global solutions_bkt
+    global output_solutions
+    global count
+    global counter
+    N = couples
+    q = [0] * N
+    states = list()
+    solutions_bkt = list()
+    output_solutions = list()
+    count = 0
+    counter = 0
+    move(q, 0, mp, wp)
+    N = 0
+
+    return random.choice(output_solutions), 0
+
+
+def create_bkt_preferences(preferences_men,preferences_women):
+    males=[]
+    women=[]
+    for key,values in preferences_men.items():
+        women_list=[]
+        for value in values:
+            woman= ord (value)-65
+            women_list.append(woman)
+        males.append(women_list)
+    for key,values in preferences_women.items():
+        men_list=[]
+        for value in values:
+            if value != '10':
+                man= ord (value)-49
+                men_list.append(man)
+            else:
+                man = 10
+                men_list.append(man)
+        women.append(men_list)
+    mp=array(males)
+    wp=array(women)
+    return mp,wp
+
+
+def solve_problem(number_of_couples,preferences_men, preferences_women, solving_metho):
+    men = create_person_list(preferences_men)
+    women = create_person_list(preferences_women)
+
+    if solving_metho == 'Greedy':
+        return greedy_approach(men, women, preferences_men, preferences_women)
+    else:
+        mp,wp = create_bkt_preferences(preferences_men,preferences_women)
+        print(mp)
+        print(wp)
+        solutie1,solutie2 = bkt_approach(int(number_of_couples), mp, wp)
+        print(solutie1,solutie2)
+        return solutie1,solutie2
+
+
 if __name__ == '__main__':
-    #start_app()
-    pass
+    start_app()
+
